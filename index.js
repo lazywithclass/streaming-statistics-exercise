b569a8153c8db6f26f3fcb8f2824f7cc97f03c0e
lazywithclass <montecristoh@gmail.com>
2015-09-05 17:25:38 +0100

    runner

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = function() {
-  return 'hi';
-};
+var printer = require('./lib/printer');
+
+var readline = require('readline');
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', function(line){
+  // TODO avoid doing an if here as it will be needed fopr the
+  // first line only but it will be evaluated every other 999999 times
+  if (line[0] === '"') return;
+  printer.print(line);
+});
+
+rl.on('close', function() {});
